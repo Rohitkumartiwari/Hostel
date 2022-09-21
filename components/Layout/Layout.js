1a456ecfe30b48667b1e3d545ed812e2e636ff87
ROHIT KUMAR TIWARI <72463795+Rohitkumartiwari@users.noreply.github.com>
2022-09-21 14:00:36 +0530

    ChangesChanges

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Sidebar from "../Sidebar/dashboard";
+import { ToastContainer } from "react-toastify";
 const Layout = ({ children }) => {
   return (
     <div className="container-fluid">
       <div className="row ps-0 ">
         <div className="col-md-2 ps-0 pe-0">
           <Sidebar />
+          <ToastContainer />
         </div>
         <div className="col-md-10 ps-0 pe-0">
           <main>{children}</main>
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,20 @@ import React, { useState, useEffect } from "react";
 import Styles from "../styles/dashboard.module.css";
 import Images from "next/image";
 import axios from "axios";
-import { FcFullTrash } from "react-icons/fc";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { GoTrashcan } from "react-icons/go";
 import { FiEdit } from "react-icons/fi";
 import { Button, Modal, ModalFooter, ModalHeader, ModalBody } from "reactstrap";
+
 export default function index() {
   const [data, setData] = useState([]);
   const [modal, setModal] = React.useState(false);
   const [info, setInfo] = useState([]);
-  const [name, setName] = useState();
-  const [fname, setFName] = useState("");
-  const [mobile, setMobile] = useState();
-  const [address, setAddress] = useState();
-  const [image, setImage] = useState();
-  const [price, setPrice] = useState();
+
   const [message, setMessage] = useState(null);
   const toggle = () => setModal(!modal);
+
   useEffect(() => {
     axios
       .get("https://apis-new.onrender.com/users")
@@ -44,169 +42,165 @@ export default function index() {
     formData.append("price", parseInt(info.price));
     axios
       .put(`https://apis-new.onrender.com/users/${info._id}`, formData)
-      .then((res) => setMessage(res.data.message));
+      .then((res) => {
+        setMessage(res.data.message);
+        setModal(!modal);
+        toast.success("data update successfully");
+      });
   };
 
   return (
     <div className="container">
       <div className="row">
         <h3 className="text-center text-success mt-4">Student Detail</h3>
-        {message ? (
-          <h4 className="text-center">{message}</h4>
-        ) : (
-          <div className="col-md-12">
-            <div className={Styles.home_table}>
-              <table class="table table-striped">
-                <thead>
-                  <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Father Name</th>
 
-                    <th scope="col">Mobile</th>
+        <div className="col-md-12">
+          <div className={Styles.home_table}>
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">No</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Father Name</th>
 
-                    <th scope="col">Price</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {(data ? data : []).map((data, index) => {
-                    return (
-                      <tr key={index}>
-                        <th scope="row">{index + 1}</th>
-                        <td className="d-flex">
-                          <div className={Styles.img_wrapper}>
-                            <Images
-                              src={`https://apis-new.onrender.com/${data.image}`}
-                              objectFit="cover"
-                              layout="fill"
-                            />
-                          </div>
-                          <span>{data.name}</span>{" "}
-                        </td>
-                        <td>{data.FathersName}</td>
-                        <td>{data.mobile}</td>
-                        <td>{data.price}</td>
-                        <td>{new Date().toLocaleDateString()}</td>
-                        <td>
-                          <div
-                            onClick={() => del(data._id)}
-                            className="cursor-pointer"
-                          >
-                            <GoTrashcan fontSize={25}></GoTrashcan>
-                          </div>
-                          <div
-                            onClick={() => {
-                              setInfo(data);
-                              toggle();
-                            }}
-                            className="cursor-pointer"
-                          >
-                            <FiEdit fontSize={25}></FiEdit>
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-                <Modal
-                  isOpen={modal}
-                  toggle={toggle}
-                  info={info}
-                  modalTransition={{ timeout: 2000 }}
-                >
-                  <ModalHeader>Update Details</ModalHeader>
-                  <ModalBody>
-                    <div className="col-md-8 mx-auto">
-                      <div className={Styles.newStudent_form}>
-                        <div class="form-group">
-                          <label className="mb-2">Name</label>
-                          <input
-                            type="text"
-                            className="form-control"
-                            id="name"
-                            value={info.name}
-                            onChange={(e) =>
-                              setInfo({ ...info, name: e.target.value })
-                            }
+                  <th scope="col">Mobile</th>
+
+                  <th scope="col">Price</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {(data ? data : []).map((data, index) => {
+                  return (
+                    <tr key={index}>
+                      <th scope="row">{index + 1}</th>
+                      <td className="d-flex">
+                        <div className={Styles.img_wrapper}>
+                          <Images
+                            src={`https://apis-new.onrender.com/${data.image}`}
+                            objectFit="cover"
+                            layout="fill"
                           />
                         </div>
-
-                        <label className="mb-2">Fathers Name</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="mobile"
-                          value={info.FathersName}
-                          onChange={(e) =>
-                            setInfo({ ...info, FathersName: e.target.value })
-                          }
-                        />
-
-                        <label className="mb-2">Mobile</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="mobile"
-                          value={info.mobile}
-                          onChange={(e) =>
-                            setInfo({ ...info, mobile: e.target.value })
-                          }
-                        />
-                        <label className="mb-2">Price</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="price"
-                          value={info.price}
-                          onChange={(e) =>
-                            setInfo({ ...info, price: e.target.value })
-                          }
-                        />
-
-                        <label className="mb-2">Address</label>
-                        <textarea
-                          className="form-control mb-2"
-                          id="address"
-                          rows="3"
-                          value={info.address}
-                          onChange={(e) =>
-                            setInfo({ ...info, address: e.target.value })
-                          }
-                        ></textarea>
-
-                        <label className="mb-2">Student Image</label>
-                        <br />
-                        <input
-                          type="file"
-                          className="form-control-file mb-3"
-                          id="image"
-                          onChange={(e) =>
-                            setInfo({
-                              ...info,
-                              image: e.currentTarget.files[0],
-                            })
-                          }
-                        />
-                        <br />
-                        <div className="d-flex justify-content-center ">
-                          <button
-                            type="button"
-                            className="btn btn-secondary btn-md "
-                            onClick={click}
-                          >
-                            Submit
-                          </button>
+                        <span>{data.name}</span>{" "}
+                      </td>
+                      <td>{data.FathersName}</td>
+                      <td>{data.mobile}</td>
+                      <td>{data.price}</td>
+                      <td>{new Date().toLocaleDateString()}</td>
+                      <td>
+                        <div
+                          onClick={() => del(data._id)}
+                          className="cursor-pointer"
+                        >
+                          <GoTrashcan fontSize={25}></GoTrashcan>
                         </div>
+                        <div
+                          onClick={() => {
+                            setInfo(data);
+                            toggle();
+                          }}
+                          className="cursor-pointer"
+                        >
+                          <FiEdit fontSize={25}></FiEdit>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+              <Modal isOpen={modal} toggle={toggle} info={info}>
+                <ModalHeader>Update Details</ModalHeader>
+                <ModalBody>
+                  <div className="col-md-8 mx-auto">
+                    <div className={Styles.newStudent_form}>
+                      <div class="form-group">
+                        <label className="mb-2">Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="name"
+                          value={info.name}
+                          onChange={(e) =>
+                            setInfo({ ...info, name: e.target.value })
+                          }
+                        />
+                      </div>
+
+                      <label className="mb-2">Fathers Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="mobile"
+                        value={info.FathersName}
+                        onChange={(e) =>
+                          setInfo({ ...info, FathersName: e.target.value })
+                        }
+                      />
+
+                      <label className="mb-2">Mobile</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="mobile"
+                        value={info.mobile}
+                        onChange={(e) =>
+                          setInfo({ ...info, mobile: e.target.value })
+                        }
+                      />
+                      <label className="mb-2">Price</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="price"
+                        value={info.price}
+                        onChange={(e) =>
+                          setInfo({ ...info, price: e.target.value })
+                        }
+                      />
+
+                      <label className="mb-2">Address</label>
+                      <textarea
+                        className="form-control mb-2"
+                        id="address"
+                        rows="3"
+                        value={info.address}
+                        onChange={(e) =>
+                          setInfo({ ...info, address: e.target.value })
+                        }
+                      ></textarea>
+
+                      <label className="mb-2">Student Image</label>
+                      <br />
+                      <input
+                        type="file"
+                        className="form-control-file mb-3"
+                        id="image"
+                        onChange={(e) =>
+                          setInfo({
+                            ...info,
+                            image: e.currentTarget.files[0],
+                          })
+                        }
+                      />
+                      <br />
+                      <div className="d-flex justify-content-center ">
+                        <button
+                          type="button"
+                          className="btn btn-secondary btn-md "
+                          onClick={click}
+                        >
+                          Submit
+                        </button>
                       </div>
                     </div>
-                  </ModalBody>
-                </Modal>
-              </table>
-            </div>
+                  </div>
+                </ModalBody>
+              </Modal>
+            </table>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
